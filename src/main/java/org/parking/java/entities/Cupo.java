332cf83b2cd10cd981de863dba6d854abbd6afe8
Aprendiz <Aprendiz@BOGDFPCGMF4143.FormacionSena.RED>
2024-02-26 14:23:42 -0500

    Entidades lsitas para el main

--- src/main/java/org/parking/java/entities/Cupo.java
+++ src/main/java/org/parking/java/entities/Cupo.java
@@ -1,5 +1,16 @@
 package org.parking.java.entities;
 
 public class Cupo {
+    public char nombre;
+
+
+    //constructor por defecto
+    public Cupo() {
+    }
+
+    //cosntructor parametrizados
+    public Cupo(char nombre) {
+        this.nombre = nombre;
+    }
 
 }
--- src/main/java/org/parking/java/entities/Pago.java
+++ src/main/java/org/parking/java/entities/Pago.java
@@ -1,5 +1,28 @@
 package org.parking.java.entities;
 
+import java.time.LocalDateTime;
+
 public class Pago {
+    //Atributos intrinsecos
+    public LocalDateTime fechaInicio;
+    public LocalDateTime fechaHoraFin;
+    public double valor;
+
+    //atributosde relación:
+    public Vehiculo vehiculo;
+    public Cupo cupo;
+    //constructor por defecto
+    public Pago() {
+    }
+    public Pago(LocalDateTime fechaInicio, LocalDateTime fechaHoraFin, double valor, Vehiculo vehiculo, Cupo cupo) {
+        this.fechaInicio = fechaInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.valor = valor;
+        this.vehiculo = vehiculo;
+        this.cupo = cupo;
+    }
+    
+
+    
 
 }
